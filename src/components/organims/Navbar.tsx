808e490d49d7d27ba779e099c2fcb2427d0123b5
Yubli Audy Warokka <72744397+yubliwarokkaid@users.noreply.github.com>
2024-12-07 13:25:33 +0800

    update navbar

--- src/components/organims/Navbar.tsx
+++ src/components/organims/Navbar.tsx
@@ -1,13 +1,13 @@
 import Link from "next/link";
-import { TbCodeDots } from "react-icons/tb";
+import { BiSolidFileJson } from "react-icons/bi";
 
 export default function Navbar() {
   return (
     <header className="bg-gradient-to-r from-sky-500 via-rose-500 to-red-500 py-6 text-white shadow-lg">
       <div className="container mx-auto flex items-center justify-between px-4">
-        <h1 className="flex items-center text-3xl font-extrabold">
-          <TbCodeDots className="mr-2" />
-          Alkitab Nusanatar API
+        <h1 className="flex items-center font-mono text-3xl font-extrabold">
+          <BiSolidFileJson className="mr-2" />
+          {`{Alkitab Nusanatar API}`}
         </h1>
         <nav className="space-x-6">
           <Link href="#endpoints" className="text-white hover:underline">
